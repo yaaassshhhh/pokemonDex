76312c38d130802317f0e2ea8e5991d02821d10c
Yash Agrawal <yashagrawal0916@gmail.com>
2024-10-13 08:02:12 +0530

    minor fix

--- src/PokemonCards.jsx
+++ src/PokemonCards.jsx
@@ -29,21 +29,21 @@ export const PokemonCards = ({ pokemonData }) => {
 
       <div className="grid-three-cols">
         <div className="pokemon-info">
-          <p>{pokemonData.base_experience}</p>
           <span> Experience:</span>
+          <p>{pokemonData.base_experience}</p>
         </div>
         <div className="pokemon-info">
-          <p>{pokemonData.stats[1].base_stat}</p>
           <span>Attack:</span>
+          <p>{pokemonData.stats[1].base_stat}</p>
         </div>
         <div className="pokemon-info">
+          <span> Abilities: </span>
           <p>
             {pokemonData.abilities
               .map((abilityInfo) => abilityInfo.ability.name)
               .slice(0, 1)
               .join(", ")}
           </p>
-          <span> Abilities: </span>
         </div>
       </div>
     </li>
